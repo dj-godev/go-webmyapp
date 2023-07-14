10a3188795cd1dfadb4d2618ad7afbfd740729d7
dj-godev <djgolang@gmail.com>
2023-07-14 23:17:18 +0530

    working code also checked null values from database

--- main.go
+++ main.go
@@ -9,26 +9,27 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Course represents a course entity
-type Course struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Units []Unit `json:"units"`
+// CourseResponse represents a course response with associated units and lessons
+type CourseResponse struct {
+	ID    int             `json:"id"`
+	Title string          `json:"title"`
+	Units []*UnitResponse `json:"units"`
 }
 
-// Unit represents a unit entity
-type Unit struct {
-	ID      int      `json:"id"`
-	Title   string   `json:"title"`
-	Lessons []Lesson `json:"lessons"`
+// UnitResponse represents a unit response with associated lessons
+type UnitResponse struct {
+	ID      *int      `json:"id,omitempty"`
+	Title   *string   `json:"title,omitempty"`
+	Lessons []*Lesson `json:"lessons"`
 }
 
 // Lesson represents a lesson entity
 type Lesson struct {
-	ID       int    `json:"id"`
-	UnitID   int    `json:"unit_id"`
-	Title    string `json:"title"`
-	Duration int    `json:"duration"`
+	ID         *int    `json:"id,omitempty"`
+	UnitID     *int    `json:"unit_id,omitempty"`
+	Title      *string `json:"title,omitempty"`
+	Duration   *int    `json:"duration,omitempty"`
+	Difficulty *string `json:"difficulty,omitempty"`
 }
 
 func main() {
@@ -52,15 +53,15 @@ func main() {
 				units.name,
 				lessons.id,
 				lessons.unit_id,
-				lessons.name,
-				lessons.duration
+				lessons.name
 			FROM
 				courses
-			INNER JOIN
+			LEFT JOIN
 				units ON courses.id = units.course_id
-			INNER JOIN
-				lessons ON units.id = lessons.unit_id
+			LEFT JOIN
+				lessons ON units.id = lessons.unit_id order by courses.id asc
 		`
+
 		rows, err := db.Query(query)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -68,17 +69,16 @@ func main() {
 		}
 		defer rows.Close()
 
-		courses := make(map[int]*Course)
+		courses := make(map[int]*CourseResponse)
 
 		for rows.Next() {
 			var courseID int
 			var courseTitle string
-			var unitID int
-			var unitTitle string
-			var lessonID int
-			var lessonUnitID int
-			var lessonTitle string
-			var lessonDuration int
+			var unitID sql.NullInt64
+			var unitTitle sql.NullString
+			var lessonID sql.NullInt64
+			var lessonUnitID sql.NullInt64
+			var lessonTitle sql.NullString
 
 			err := rows.Scan(
 				&courseID,
@@ -88,7 +88,6 @@ func main() {
 				&lessonID,
 				&lessonUnitID,
 				&lessonTitle,
-				&lessonDuration,
 			)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -96,37 +95,69 @@ func main() {
 			}
 
 			if _, ok := courses[courseID]; !ok {
-				courses[courseID] = &Course{
+				courses[courseID] = &CourseResponse{
 					ID:    courseID,
 					Title: courseTitle,
+					Units: []*UnitResponse{},
 				}
 			}
 
-			unit := Unit{
-				ID:    unitID,
-				Title: unitTitle,
+			unit := getUnitByID(courses[courseID].Units, int(unitID.Int64))
+			if unit == nil {
+				unit = &UnitResponse{
+					ID:      getIntPointer(unitID),
+					Title:   getStringPointer(unitTitle),
+					Lessons: []*Lesson{},
+				}
+				courses[courseID].Units = append(courses[courseID].Units, unit)
 			}
 
-			lesson := Lesson{
-				ID:       lessonID,
-				UnitID:   lessonUnitID,
-				Title:    lessonTitle,
-				Duration: lessonDuration,
+			lesson := &Lesson{
+				ID:     getIntPointer(lessonID),
+				UnitID: getIntPointer(lessonUnitID),
+				Title:  getStringPointer(lessonTitle),
 			}
 
-			courses[courseID].Units = append(courses[courseID].Units, unit)
-			courses[courseID].Units[unitID].Lessons = append(courses[courseID].Units[unitID].Lessons, lesson)
+			unit.Lessons = append(unit.Lessons, lesson)
 		}
 
-		var result []Course
+		var response []CourseResponse
 		for _, course := range courses {
-			result = append(result, *course)
+			response = append(response, *course)
 		}
 
 		// Return the courses with units and lessons as a JSON response
-		c.JSON(http.StatusOK, result)
+		c.JSON(http.StatusOK, response)
 	})
 
 	// Start the server
 	router.Run(":8082")
+}
+
+// getUnitByID returns a unit with the specified ID from the given units slice
+func getUnitByID(units []*UnitResponse, unitID int) *UnitResponse {
+	for i := range units {
+		if units[i].ID != nil && *units[i].ID == unitID {
+			return units[i]
+		}
+	}
+	return nil
+}
+
+// getIntPointer returns a pointer to an int value
+func getIntPointer(val sql.NullInt64) *int {
+	if val.Valid {
+		value := int(val.Int64)
+		return &value
+	}
+	return nil
+}
+
+// getStringPointer returns a pointer to a string value
+func getStringPointer(val sql.NullString) *string {
+	if val.Valid {
+		value := val.String
+		return &value
+	}
+	return nil
 }
